4b800d6ce2e9ef7e356c5fe9f222e7aa3ce38e21
jeremyhsimons <>
2023-08-11 19:24:23 +0000

    Add sign-in form to project.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -33,10 +33,10 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-        await axios.post('/dj-rest-auth/registration/', signUpData)
+        await axios.post('/dj-rest-auth/registration/', signUpData);
         history.push('/signin');
     } catch(err){
-        setErrors(err.response?.data)
+        setErrors(err.response?.data);
     }
   };
 
