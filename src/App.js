bb6019507db265c845f6bf3fc69289d4014a86b3
jeremyhsimons <>
2023-08-16 17:39:49 +0000

    Wire up post home page to api and filters.

--- src/App.js
+++ src/App.js
@@ -19,21 +19,34 @@ function App() {
         <NavBar />
         <Container className={styles.Main}>
             <Switch>
-                <Route exact path="/" render={()=> <PostsPage message="No results found. Adjust the search keyword."/>} />
+                <Route 
+                    exact 
+                    path="/" 
+                    render={()=> (
+                    <PostsPage message="No results found. Adjust the search keyword."/>
+                    )}
+                     />
                 <Route 
                     exact 
                     path="/feed" 
-                    render={()=> <PostsPage 
-                                    message="No results found. Adjust the search keyword or follow a user."/>}
-                                    filter={`owner__followed__owner__profile=${profile_id}&`} 
-                    />
+                    render={()=> (
+                        <PostsPage 
+                            message="No results found. Adjust the search keyword or follow a user."
+                            filter={`owner__followed__owner__profile=${profile_id}&`}
+                        />
+                    )}
+                />
                 <Route 
                     exact 
                     path="/liked" 
-                    render={()=> <PostsPage 
-                                    message="No results found. Adjust the search keyword or like a post."/>}
-                                    filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} 
-                    />
+                    render={()=> (
+                        <PostsPage 
+                            message="No results found. Adjust the search keyword or like a post."
+                            filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} 
+                        />
+                    )}
+                />
+
                 <Route exact path="/signin" render={()=> <SignInForm />} />
                 <Route exact path="/signup" render={()=> <SignUpForm />} />
                 <Route exact path="/posts/create" render={() => <PostCreateForm />}/>
